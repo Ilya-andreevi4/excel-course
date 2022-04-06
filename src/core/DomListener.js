333969d8845db8ea4b35ec1f691ef189d126459b
Orekhov Ilya Andreevich <ilya-andreevi4@mail.ru>
2022-04-06 18:34:36 +0500

    Framework created DOMListeners

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,36 @@
-export class DomListener {
+import {capitalize} from './utilits'
 
+export class DomListener {
+    constructor($root, listeners=[]) {
+        if (!$root) {
+            throw new Error(`No $root provided for DomListener!`)
+        }
+        this.$root = $root
+        this.listeners = listeners
+    }
+
+    initDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener)
+            if (!this[method]) {
+                throw new Error(
+                    `Method ${method} is not implemented in ${this.name}`
+                    )
+            }
+
+            this.$root.on(listener, this[method].bind(this))
+        })
+    }
+
+    removeDOMListeners() {
+        // this.listeners.forEach(this.listener => {
+        //     const method = getMethodName(listener)
+        //     this.$root.off(listener, this[method])
+        // })
+    }
+}
+
+// input => onInput
+function getMethodName(eventName) {
+    return 'on' + capitalize(eventName)
 }
